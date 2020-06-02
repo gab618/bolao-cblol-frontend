201701c24a5d7a5e17988d59511ce30e995fe133
Gabriel <knuckles4x@gmail.com>
2020-06-02 04:36:01 -0300

    auth validation

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import { Form, Input } from '@rocketseat/unform';
 import { Link } from 'react-router-dom';
+import * as Yup from 'yup';
+
+const schema = Yup.object().shape({
+  email: Yup.string()
+    .email('Insira um e-mail válido')
+    .required('O e-mail é obrigatório'),
+  password: Yup.string().required('A senha é obrigatória'),
+});
 
 export default function SignIn() {
   return (
     <>
       <h1>Bolão CBLOL</h1>
-      <Form>
+      <Form schema={schema}>
         <Input name="email" type="email" placeholder="Seu e-mail" />
         <Input name="password" type="password" placeholder="Senha" />
 
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import { Form, Input } from '@rocketseat/unform';
 import { Link } from 'react-router-dom';
+import * as Yup from 'yup';
+
+const schema = Yup.object().shape({
+  name: Yup.string().required('O nome é obrigatório'),
+  email: Yup.string()
+    .email('Insira um e-mail válido')
+    .required('O e-mail é obrigatório'),
+  password: Yup.string()
+    .min(6, 'No mínimo 6 caracteres')
+    .required('A senha é obrigatória'),
+});
 
 export default function SignUp() {
   return (
     <>
       <h1>Crie sua conta</h1>
-      <Form>
+      <Form schema={schema}>
         <Input name="name" placeholder="Nome" />
         <Input name="email" type="email" placeholder="E-mail" />
         <Input name="password" type="password" placeholder="Senha" />
