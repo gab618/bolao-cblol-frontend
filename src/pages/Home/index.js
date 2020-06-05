659518b36a8b38d7564563165629a8f62e1f7ecb
Gabriel <knuckles4x@gmail.com>
2020-06-05 14:56:32 -0300

    bets working with radio button

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,13 @@
 import React, { useState, useMemo, useEffect } from 'react';
 import { toast } from 'react-toastify';
-import { format, subDays, addDays, getHours, parseISO } from 'date-fns';
+import {
+  format,
+  subDays,
+  addDays,
+  getHours,
+  parseISO,
+  isAfter,
+} from 'date-fns';
 import pt from 'date-fns/locale/pt';
 import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
 import Radio from '@material-ui/core/Radio';
@@ -11,18 +18,42 @@ import { Container, Match } from './styles';
 
 export default function Home() {
   const [date, setDate] = useState(new Date());
+  const [bets, setBets] = useState([]);
   const [round, setRound] = useState({});
+
+  async function loadBets() {
+    const responseBets = await api.get('bets');
+    setBets(responseBets.data);
+  }
+  useEffect(() => {
+    loadBets();
+  }, []);
 
   useEffect(() => {
     async function loadRound() {
-      const response = await api.get('schedule', {
+      const responseSchedule = await api.get('schedule', {
         params: { date },
       });
-      setRound(response.data);
-      console.log(response.data);
+      if (!!responseSchedule.data && !!responseSchedule.data.Matches) {
+        const formatedMatches = responseSchedule.data.Matches.map((m) => {
+          const past = isAfter(new Date(), parseISO(m.start_time));
+          const start_hour = { start_hour: getHours(parseISO(m.start_time)) };
+          const bet = bets.filter((b) => b.match_id === m.id);
+          return Object.assign(
+            m,
+            start_hour,
+            { choice: bet[0].choice },
+            { past }
+          );
+        });
+        setRound(
+          Object.assign(responseSchedule.data, { Matches: formatedMatches })
+        );
+      }
+      setRound(responseSchedule.data);
     }
     loadRound();
-  }, [date]);
+  }, [date, bets]);
 
   const dateFormatted = useMemo(
     () => format(date, "d 'de' MMMM", { locale: pt }),
@@ -33,6 +64,7 @@ export default function Home() {
     async function bet() {
       try {
         await api.post(`bet/${match}`, { choice });
+        await loadBets();
         toast.success('Aposta computada com sucesso');
       } catch (err) {
         toast.error('Error ao registrar aposta :(');
@@ -62,13 +94,14 @@ export default function Home() {
       <ul>
         {!!round && !!round.Matches
           ? round.Matches.map((m) => (
-              <Match>
-                <strong>{getHours(parseISO(m.start_time))}h</strong>
+              <Match key={m.id} past={m.past}>
+                <strong>{m.start_hour}h</strong>
                 <RadioGroup row className="teams">
                   <div className="team">
-                    <img src={m.blue.image} alt="" />
+                    <img src={m.blue.image} alt={m.blue.name} />
                     <span>{m.blue.code}</span>
                     <Radio
+                      checked={m.blue.id === m.choice}
                       onChange={() => handleChange(m.id, m.blue.id)}
                       value={m.blue.id}
                       name="radio-button-demo"
@@ -76,9 +109,10 @@ export default function Home() {
                   </div>
                   X
                   <div className="team">
-                    <img src={m.red.image} alt="" />
+                    <img src={m.red.image} alt={m.red.name} />
                     <span>{m.red.code}</span>
                     <Radio
+                      checked={m.red.id === m.choice}
                       onChange={() => handleChange(m.id, m.red.id)}
                       value={m.red.id}
                       name="radio-button-demo"
@@ -89,7 +123,6 @@ export default function Home() {
             ))
           : 'Sem jogos nesse dia :('}
       </ul>
-      <button type="button">Apostar!</button>
     </Container>
   );
 }
