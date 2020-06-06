5521dee59e3e3800d06d99b693b1daf628affdc0
Gabriel <knuckles4x@gmail.com>
2020-06-06 20:38:48 -0300

    table pagination and style

--- src/pages/Leaderboard/index.js
+++ src/pages/Leaderboard/index.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
+import PropTypes from 'prop-types';
 
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
 import Avatar from '@material-ui/core/Avatar';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
@@ -9,22 +10,129 @@ import TableCell from '@material-ui/core/TableCell';
 import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
+import TableFooter from '@material-ui/core/TableFooter';
+import TablePagination from '@material-ui/core/TablePagination';
 import Paper from '@material-ui/core/Paper';
+import IconButton from '@material-ui/core/IconButton';
+import {
+  MdFirstPage,
+  MdKeyboardArrowLeft,
+  MdKeyboardArrowRight,
+  MdLastPage,
+} from 'react-icons/md';
 import { Wrapper } from './styles';
 import api from '../../services/api';
 
-const useStyles = makeStyles({
-  table: {
-    minWidth: 400,
-    background: '#fff',
+const useStyles1 = makeStyles((theme) => ({
+  root: {
+    flexShrink: 0,
+    marginLeft: theme.spacing(2.5),
   },
-  drawerPaper: { background: '#2c3036' },
+}));
+
+function TablePaginationActions(props) {
+  const classes = useStyles1();
+  const theme = useTheme();
+  const { count, page, rowsPerPage, onChangePage } = props;
+
+  const handleFirstPageButtonClick = (event) => {
+    onChangePage(event, 0);
+  };
+
+  const handleBackButtonClick = (event) => {
+    onChangePage(event, page - 1);
+  };
+
+  const handleNextButtonClick = (event) => {
+    onChangePage(event, page + 1);
+  };
+
+  const handleLastPageButtonClick = (event) => {
+    onChangePage(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
+  };
+
+  return (
+    <div className={classes.root}>
+      <IconButton
+        onClick={handleFirstPageButtonClick}
+        disabled={page === 0}
+        aria-label="first page"
+      >
+        {theme.direction === 'rtl' ? (
+          <MdLastPage color="white" />
+        ) : (
+          <MdFirstPage color="white" />
+        )}
+      </IconButton>
+      <IconButton
+        onClick={handleBackButtonClick}
+        disabled={page === 0}
+        aria-label="previous page"
+      >
+        {theme.direction === 'rtl' ? (
+          <MdKeyboardArrowRight color="white" />
+        ) : (
+          <MdKeyboardArrowLeft color="white" />
+        )}
+      </IconButton>
+      <IconButton
+        onClick={handleNextButtonClick}
+        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+        aria-label="next page"
+      >
+        {theme.direction === 'rtl' ? (
+          <MdKeyboardArrowLeft color="white" />
+        ) : (
+          <MdKeyboardArrowRight color="white" />
+        )}
+      </IconButton>
+      <IconButton
+        onClick={handleLastPageButtonClick}
+        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+        aria-label="last page"
+      >
+        {theme.direction === 'rtl' ? (
+          <MdFirstPage color="white" />
+        ) : (
+          <MdLastPage color="white" />
+        )}
+      </IconButton>
+    </div>
+  );
+}
+TablePaginationActions.propTypes = {
+  count: PropTypes.number.isRequired,
+  onChangePage: PropTypes.func.isRequired,
+  page: PropTypes.number.isRequired,
+  rowsPerPage: PropTypes.number.isRequired,
+};
+
+const useStyles2 = makeStyles({
+  table: {
+    minWidth: 360,
+    margin: 'auto',
+  },
 });
 
 export default function Leaderboard() {
   const profile = useSelector((state) => state.user.profile);
+  const classes = useStyles2();
   const [users, setUsers] = useState([]);
   const [points, setPoints] = useState('');
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+
+  const emptyRows =
+    rowsPerPage - Math.min(rowsPerPage, users.length - page * rowsPerPage);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
 
   useEffect(() => {
     async function getUsers() {
@@ -39,13 +147,11 @@ export default function Leaderboard() {
     getPoints();
   }, []);
 
-  const classes = useStyles();
-
   return (
     <Wrapper>
       <span>Sua pontuação: {points}</span>
       <TableContainer component={Paper}>
-        <Table className={classes.table} aria-label="simple table">
+        <Table className={classes.table} aria-label="leaderboard table">
           <TableHead>
             <TableRow>
               <TableCell />
@@ -54,7 +160,13 @@ export default function Leaderboard() {
             </TableRow>
           </TableHead>
           <TableBody>
-            {users.map((row) => (
+            {(rowsPerPage > 0
+              ? users.slice(
+                  page * rowsPerPage,
+                  page * rowsPerPage + rowsPerPage
+                )
+              : users
+            ).map((row) => (
               <TableRow key={row.id}>
                 <TableCell align="center">
                   <Avatar alt={row.name} src={row.avatar && row.avatar.url} />
@@ -63,7 +175,31 @@ export default function Leaderboard() {
                 <TableCell align="right">{row.points}</TableCell>
               </TableRow>
             ))}
+            {emptyRows > 0 && (
+              <TableRow style={{ height: 53 * emptyRows }}>
+                <TableCell colSpan={6} />
+              </TableRow>
+            )}
           </TableBody>
+          <TableFooter>
+            <TableRow>
+              <TablePagination
+                rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
+                colSpan={3}
+                count={users.length}
+                rowsPerPage={rowsPerPage}
+                page={page}
+                SelectProps={{
+                  inputProps: { 'aria-label': 'rows per page' },
+                  native: true,
+                }}
+                onChangePage={handleChangePage}
+                onChangeRowsPerPage={handleChangeRowsPerPage}
+                ActionsComponent={TablePaginationActions}
+                labelRowsPerPage="Usuários por página"
+              />
+            </TableRow>
+          </TableFooter>
         </Table>
       </TableContainer>
     </Wrapper>
--- src/pages/Leaderboard/styles.js
+++ src/pages/Leaderboard/styles.js
@@ -1,10 +1,11 @@
 import styled from 'styled-components';
 
 export const Wrapper = styled.div`
-  max-width: 900px;
+  max-width: 920px;
   margin: 50px auto;
   display: flex;
   flex-direction: column;
+  padding: 0 20px;
 
   span {
     font-weight: bold;
@@ -16,9 +17,29 @@ export const Wrapper = styled.div`
   th {
     font-size: 18px;
     font-weight: bold;
+    color: #fff;
   }
 
   td {
     font-size: 18px;
+    color: #fff;
+  }
+
+  table,
+  td,
+  th {
+    border-bottom: 1px solid rgb(87, 95, 104);
+  }
+
+  .MuiPaper-root {
+    background-color: #2c3036;
+  }
+
+  .MuiTablePagination-selectIcon {
+    color: #fff;
+  }
+
+  .MuiTablePagination-menuItem {
+    color: #000;
   }
 `;
