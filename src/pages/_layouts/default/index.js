a578229845e342c0f0e6736fe25ad4a8f9deefe3
Gabriel <knuckles4x@gmail.com>
2020-06-13 19:17:35 -0300

    check if token is still valid

--- src/pages/_layouts/default/index.js
+++ src/pages/_layouts/default/index.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
+import { useSelector, useDispatch } from 'react-redux';
+import jwt from 'jsonwebtoken';
+
+import { signOut } from '../../../store/modules/auth/actions';
 
 import { Wrapper } from './styles';
 import Header from '../../../components/Header';
 
 export default function DefaultLayout({ children }) {
+  const dispatch = useDispatch();
+  const myToken = useSelector((state) => state.auth.token);
+
+  useEffect(() => {
+    function checkToken(token) {
+      if (token && jwt.decode(token)) {
+        const expiry = jwt.decode(token).exp;
+        const now = new Date();
+        if (now.getTime() > expiry * 1000) {
+          dispatch(signOut());
+        }
+      }
+    }
+    checkToken(myToken);
+  }, [children, dispatch, myToken]);
+
   return (
     <Wrapper>
       <Header />
